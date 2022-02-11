324a7b7a9a3d178904e892f73f452747dd52fb28
hmcvay <hayleymcvay8@gmail.com>
2022-02-11 13:58:46 -0800

    WIP loop: accessed the correct object, however, value returns undefined and NaN

--- src/index.js
+++ src/index.js
@@ -16,19 +16,20 @@ function getElements(response) {
     const multArray = [];
     $("ul#currency").append(`<li>Currency</li>`);
     $("ul#rate").append(`<li>Conversion Rate</li>`);
-    Object.entries(response).forEach((entry) => {
-      const [key, value] = entry;
-      currenciesArray.push(entry);
-      console.log(currenciesArray);
-    });
-    // for (let i = 0; i < 171; i++) {
-    //   const currency = response.conversion_rates[0][i];
-    //   const rate = response.converstion_rates[1][i];
-    //   $("ul#currency").append(`<li>${currency}</li>`);
-    //   $("ul#rate").append(`<li>${rate}</li>`);
-    //   multArray.push([currency, rate]);
-    //   currenciesArray.push(currency);
-    //   ratesArray.push(rate);
+    for (let i = 0; i < 171; i++) {
+      const currency = response.conversion_rates[i];
+      const rate = response.conversion_rates[i]+1;
+      $("ul#currency").append(`<li>${currency}</li>`);
+      $("ul#rate").append(`<li>${rate}</li>`);
+      multArray.push([currency, rate]);
+      currenciesArray.push(currency);
+      ratesArray.push(rate);
+    // Object.entries(response.conversion_rates).forEach((entry) => {
+    //   const [key, value] = entry;
+    //   currenciesArray.push(entry);
+    //   console.log(currenciesArray);
+    }
+    
   } else {
     $(".showErrors").text(`There was an error: ${response}`);
   }
