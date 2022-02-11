5e67fd8c2a64def2262d4d1a9cbac06a38b7a178
hmcvay <hayleymcvay8@gmail.com>
2022-02-11 09:42:12 -0800

    BREAK: 20 min break. re-read API call lesson. Add API call to currency.js with fetch.

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,3 +1,13 @@
 export default class Currency {
-  
+  static async getCurrency(currency) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6${process.env.API_KEY}/latest/${currency}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch (error) {
+      return error.message;
+    }
+  }
 }
