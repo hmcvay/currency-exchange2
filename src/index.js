cbd1d2f109f716fac357480458c6ce71dcca57f2
hmcvay <hayleymcvay8@gmail.com>
2022-02-11 13:50:05 -0800

    WIP: loop. loop returns key value entries of wrong object - need entries of further nested object.

--- src/index.js
+++ src/index.js
@@ -6,25 +6,35 @@ import Currency from "./js/currency.js";
 async function makeApiCall() {
   const response = await Currency.getCurrency();
   getElements(response);
-  console.log(response);
 }
 
-let currencyArray = [];
+let currenciesArray = [];
+let ratesArray = [];
 
 function getElements(response) {
   if (response) {
-    for (let i = 0; i < 171; i++) {
-      const currency = response.conversion_rates[i];
-      currencyArray.push(currency);
-    }
-    $(".showResponse").text(currencyArray);    
+    const multArray = [];
+    $("ul#currency").append(`<li>Currency</li>`);
+    $("ul#rate").append(`<li>Conversion Rate</li>`);
+    Object.entries(response).forEach((entry) => {
+      const [key, value] = entry;
+      currenciesArray.push(entry);
+      console.log(currenciesArray);
+    });
+    // for (let i = 0; i < 171; i++) {
+    //   const currency = response.conversion_rates[0][i];
+    //   const rate = response.converstion_rates[1][i];
+    //   $("ul#currency").append(`<li>${currency}</li>`);
+    //   $("ul#rate").append(`<li>${rate}</li>`);
+    //   multArray.push([currency, rate]);
+    //   currenciesArray.push(currency);
+    //   ratesArray.push(rate);
   } else {
     $(".showErrors").text(`There was an error: ${response}`);
   }
 }
 
 makeApiCall();
-console.log(currencyArray);
 
 // $(document).ready(function() {
 //   let currency = "";
