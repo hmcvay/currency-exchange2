c5d7081bdddf13b1510335149a865474a7e506cb
hmcvay <hayleymcvay8@gmail.com>
2022-02-11 11:39:24 -0800

    BUGFIX: typo in api call, missing /, which resulted in CORS error. Call now completes.

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,7 +1,7 @@
 export default class Currency {
   static async getCurrency() {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6${process.env.API_KEY}/latest/USD`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       if (!response.ok) {
         throw Error(response.statusText);
       }
