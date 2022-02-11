c1f81daee3fa5894ef685f78d52d404a2ba9c196
hmcvay <hayleymcvay8@gmail.com>
2022-02-11 09:54:55 -0800

    Add function to make api call in index.js and update lint rule.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import $ from "jquery";
 import "./css/styles.css";
 import Currency from "./js/currency.js";
+
+async function makeApiCall(currency) {
+  const response = await Currency.getCurrency(currency);
+  getElements(response);
+}
+
+function getElements(response) {
+  if (response) {
+    $(".showResponse").text(`${response}`);
+  } else {
+    $(".showErrors").text(`There was an error: ${response}`);
+  }
+}
